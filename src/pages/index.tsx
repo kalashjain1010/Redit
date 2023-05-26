51237ad973743a55c507b46ae080fd378f228a96
kalash jain <kalashjain54@gmail.com>
2023-05-26 10:34:29 +0530

    finalizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,7 @@ const Home: NextPage = () => {
         /**
          * queryResults is an array of length 3, each with 0-2 posts from
          * 3 communities that the user has joined
-         * /
+         * 
          */
         queryResults.forEach((result) => {
           const posts = result.docs.map((doc) => ({
